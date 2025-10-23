52ed44c0532bf02085afd860dbb4c142c4c8756d
Vyach Mayorskiy <vmayorskiy@acrotron.com>
2025-10-23 10:04:54 +0000

    Replacing file-mask flag with '--include' ('-i')

--- src/aye/__main__.py
+++ src/aye/__main__.py
@@ -122,7 +122,7 @@ def chat(
         None, "--root", "-r", help="Root folder where source files are located."
     ),
     file_mask: str = typer.Option(
-        None, "--file-mask", "-m", help="File mask for source files to include into generation. Comma-separated masks are allowed."
+        None, "--include", "-i", help="Include patterns for source files to include into generation. Comma-separated globs are allowed."
     ),
 ):
     """
@@ -131,7 +131,7 @@ def chat(
     Examples: \n
     aye chat \n
     aye chat --root ./src \n
-    aye chat --file-mask "*.js" --root ./frontend \n
+    aye chat --include "*.js,*.html" --root ./frontend \n
     """
     handle_chat(root, file_mask)
 
