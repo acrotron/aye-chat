852c2dcece80053315831ca869081f41d8105126
Vyach Mayorskiy <vmayorskiy@acrotron.com>
2025-10-25 14:44:45 +0000

    api tests added

--- tests/python_path.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-# Get the absolute path of the src subfolder
-src_subfolder_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../src', 'aye'))
-
-# Add the src subfolder to the system path
-sys.path.append(src_subfolder_path)
-
